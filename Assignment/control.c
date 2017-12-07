1cadd7dc1037d5aa81c77eed4d7fcc7d2c21dcd4
Tom Marshall <notroot@exlercs.com>
2017-12-07 15:50:15 +0000

    Update

--- Assignment/control.c
+++ Assignment/control.c
@@ -1,24 +1,71 @@
+/*
+* control.c
+*
+* contains code for reading buttons
+* and writing data to LEDs
+*
+* Author: Thomas Marshall (B00705297)
+* Date: 04/12/17
+*
+*/
+ 
 #include "control.h"
-void increaseToggle(){
+
+int ledtoggleTime=2500000;
+int colour=GREEN;
+void setup()
+{
+    SYSCTL_RCGCGPIO_R=0x20U;
+    GPIO_PORTF_LOCK_R=0x4C4F434BU;
+    GPIO_PORTF_CR_R=0xFF;
+    GPIO_PORTF_DIR_R=RED|GREEN|BLUE;
+    GPIO_PORTF_PUR_R=0x11U;
+    GPIO_PORTF_DEN_R=0x1FU;
 }
-void decreaseToggle(){
-}
-void listen(){
-	GPIO_PORTF_DATA_R&=(~0x0EU);
-		switch(GPIO_PORTF_DATA_R & 0x11U){
-			case 0x00:
-				GPIO_PORTF_DATA_R |=RED;
-				break;
+void listen()
+{
+    switch(GPIO_PORTF_DATA_R & 0x11U)
+		{
+		    case 0x00: //Both Buttons
+				    colour=RED;
+				    break;
 			
-			case 0x01:
-				GPIO_PORTF_DATA_R |=BLUE;
-				break;
+			    case 0x01: //Right Button
+				    if(ledtoggleTime>2500000)
+				    {
+					    colour=GREEN;
+					    ledtoggleTime/=1.25;
+				    }
+			  	break;
 			
-			case 0x10:
-				GPIO_PORTF_DATA_R |=GREEN;
-				break;
+		  	case 0x10: //Left Button
+				  if(ledtoggleTime<12500000)
+				  {
+					  colour=GREEN;
+				  	ledtoggleTime*=1.25;
+			  	}
+				
+				  break;
+				
+			  default:
+			  	colour=GREEN;
+		  	break;
 		}
+		  flashled(ledtoggleTime, colour);
 }
-void setLED(){
-	GPIO_PORTF_DATA_R |=GREEN;
+void flashled(int speed, int colour)
+{
+	  GPIO_PORTF_DATA_R&=(~0x0EU);
+	  delay(speed);
+	  GPIO_PORTF_DATA_R=colour;
+	  delay(speed);
+}
+
+void delay(int time)
+{
+    int delay=0;
+	  while (delay < time)
+	  {
+		  delay++;
+	  }
 }
--- Assignment/control.h
+++ Assignment/control.h
@@ -1,14 +1,26 @@
-#define SYSCTL_RCGCGPIO_R       (*((volatile unsigned long *)0x400FE608))
-#define GPIO_PORTF_LOCK_R       (*((volatile unsigned long *)0x40025520))
-#define GPIO_PORTF_CR_R         (*((volatile unsigned long *)0x40025524))
-#define GPIO_PORTF_DIR_R        (*((volatile unsigned long *)0x40025400))
-#define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
-#define GPIO_PORTF_DEN_R        (*((volatile unsigned long *)0x4002551C))
-#define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
+/* 
+* control.h
+*
+* contains definitions for registers
+* and declarations for functions
+*
+* Author: Thomas Marshall (B00705297)
+* Date: 04/12/17
+*
+*/
+
+#define SYSCTL_RCGCGPIO_R	(*((volatile unsigned long *)0x400FE608))
+#define GPIO_PORTF_LOCK_R (*((volatile unsigned long *)0x40025520))
+#define GPIO_PORTF_CR_R   (*((volatile unsigned long *)0x40025524))
+#define GPIO_PORTF_DIR_R  (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_PUR_R  (*((volatile unsigned long *)0x40025510))
+#define GPIO_PORTF_DEN_R  (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_DATA_R (*((volatile unsigned long *)0x400253FC))
 #define RED (1U<<1)
 #define BLUE (1U<<2)
 #define GREEN (1U<<3)
-void increaseToggle();
-void decreaseToggle();
-void setLED();
+#define YELLOW (RED && GREEN)
+void setup();
+void delay(int time);
+void flashled();
 void listen();
